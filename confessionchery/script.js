9f308b05d0dde65bd190f8f0508babfcebe36f36
Austin Berbereia <austin.berbereia@gmail.com>
2017-05-08 21:04:03 -0700

    Initial commit. Include already finished project code.

---
+++ confessionchery/script.js
@@ -0,0 +1,9 @@
+$(document).click(
+  function(event){
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");
+    var _opened = $navbar.hasClass("in");
+    if(_opened === true && !clickover.hasClass("navbar-toggle")){
+      $("button.navbar-toggle").click();
+    }
+  });
