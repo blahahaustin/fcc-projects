d1608ab3033b7c179cf16ef74a6473e94edb92ce
blahahaustin <austin.berbereia@gmail.com>
2017-08-05 21:26:44 -0700

    Finished, may go back and edit the stype of the page, but the functionality is there.

---
+++ random-quote-machine/script.js
@@ -0,0 +1,31 @@
+//Only run the code after the document is ready.
+$(document).ready(function() {
+
+    //Initial call of the function to open the page with a quote.
+    getQuote();
+
+    //Call the getQuote function when the button is clicked.
+    $('#get-quote').on('click', function() {
+        getQuote();
+    });
+
+});
+
+//Function makes an AJAX call to a quotes API and inserts the data into our HTML.
+function getQuote() {
+    $.ajax({
+        url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+        success: function(data) {
+            var post = data.shift(); // The data is an array of posts. Grab the first one.
+
+            //The content of the quote.
+            var quoteContent = "\"" + post.content.replace(/<p>|<\/p>/g, "") + "\"";
+            $('#quote-content').html(quoteContent);
+
+            //The author of the quote.
+            var quoteAuthor = "- " + post.title;
+            $('#quote-title').text(quoteAuthor);
+        },
+        cache: false
+    });
+}
