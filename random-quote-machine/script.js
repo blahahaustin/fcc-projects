8096ea38f64adffcb1c6a7be3ceef5009d973dcb
blahahaustin <austin.berbereia@gmail.com>
2017-08-06 18:09:47 -0700

    Overhauled to make the code more modular. Added the tweet button. Calling this finished for now!

--- random-quote-machine/script.js
+++ random-quote-machine/script.js
@@ -1,31 +1,72 @@
 //Only run the code after the document is ready.
 $(document).ready(function() {
 
-    //Initial call of the function to open the page with a quote.
-    getQuote();
+    //Need to wait until AJAX call has succeeded before we can do anything.
+    getData().success(function(data) {
 
-    //Call the getQuote function when the button is clicked.
-    $('#get-quote').on('click', function() {
-        getQuote();
+        //Put the quote data into its own variable so we can edit it.
+        var quote = data.shift();
+
+        var quoteContent = getQuoteContent(quote);
+        var quoteAuthor = getQuoteAuthor(quote);
+        var tweetIntent = getTweetIntent(quoteContent, quoteAuthor);
+        writeData(quoteContent, quoteAuthor, tweetIntent);
+
     });
 
+    //Call the getQuote function when the button is clicked, and update link addresses.
+    $('#get-quote').on('click', function() {
+        getData().success(function(data) {
+
+            //Put the quote data into its own variable so we can edit it.
+            var quote = data.shift();
+
+            var quoteContent = getQuoteContent(quote);
+            var quoteAuthor = getQuoteAuthor(quote);
+            var tweetIntent = getTweetIntent(quoteContent, quoteAuthor);
+            writeData(quoteContent, quoteAuthor, tweetIntent);
+
+        });
+    });
 });
 
-//Function makes an AJAX call to a quotes API and inserts the data into our HTML.
-function getQuote() {
-    $.ajax({
+//Function makes an AJAX call to a quotes API.
+function getData() {
+    return $.ajax({
         url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
-        success: function(data) {
-            var post = data.shift(); // The data is an array of posts. Grab the first one.
-
-            //The content of the quote.
-            var quoteContent = "\"" + post.content.replace(/<p>|<\/p>/g, "") + "\"";
-            $('#quote-content').html(quoteContent);
-
-            //The author of the quote.
-            var quoteAuthor = "- " + post.title;
-            $('#quote-title').text(quoteAuthor);
-        },
+        dataType: 'json',
         cache: false
     });
+}
+
+function getQuoteContent(data) {
+    //The content of the quote.
+    var quoteContent = data.content.replace(/\n/, "");
+    var quoteContent = "\"" + quoteContent.replace(/<p>|<\/p>/g, "").trim() + "\"";
+    console.log(quoteContent);
+    return quoteContent;
+}
+
+function getQuoteAuthor(data) {
+    //The author of the quote.
+    var quoteAuthor = "- " + data.title;
+    return quoteAuthor;
+}
+
+function getTweetIntent(quoteContent, quoteAuthor) {
+
+    //Encode for URL.
+    var webAddress = encodeURIComponent(quoteContent);
+    webAddress += "+" + encodeURIComponent(quoteAuthor);
+    webAddress.replace(/'/, "%27");
+    console.log(webAddress);
+
+    //Return the full web address for the tweet intent.
+    return "https://twitter.com/intent/tweet?text=" + webAddress;
+}
+
+function writeData(quoteContent, quoteAuthor, tweetIntent) {
+    $('#quote-content').html(quoteContent);
+    $('#quote-title').html(quoteAuthor);
+    $("#tweet-intent").attr("href", tweetIntent);
 }
