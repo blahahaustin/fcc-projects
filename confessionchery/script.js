79ef4a251bda82fbbf11a4da74f77c9b6f2e5c60
blahahaustin <austin.berbereia@gmail.com>
2017-09-07 13:23:14 -0700

    Doing some restructuring and organization.

--- confessionchery/script.js
+++
@@ -1,9 +0,0 @@
-$(document).click(
-  function(event){
-    var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");
-    var _opened = $navbar.hasClass("in");
-    if(_opened === true && !clickover.hasClass("navbar-toggle")){
-      $("button.navbar-toggle").click();
-    }
-  });
